5a804bc9981b69a0093035cee75d9d4fc0c6f846
bippolaroid <bippolaroid@proton.me>
2025-11-15 13:05:38 -0500

    design changes and slight cleanup

--- index.js
+++ index.js
@@ -2,75 +2,71 @@ const kvMap = new Map();
 const settableTypes = ["string", "number"];
 
 const mainContainer = document.querySelector("#main");
+const newFileButton = document.querySelector("#new-file");
 const openFileButton = document.querySelector("#open-file");
 
-const saveButton = document.createElement("button");
-saveButton.classList = "button primary-button";
-saveButton.textContent = "Save";
+const saveAllButton = document.createElement("button");
+saveAllButton.classList = "button primary-button";
+saveAllButton.textContent = "Save all Entries";
 
-if (openFileButton) {
-    openFileButton.addEventListener("click", () => {
-        const fileInput = document.createElement("input");
-        fileInput.type = "file";
-        fileInput.accept = "application/json"
-        fileInput.style.display = "none";
-        fileInput.click();
+openFileButton?.addEventListener("click", () => {
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.accept = "application/json"
+    fileInput.style.display = "none";
+    fileInput.click();
 
-        fileInput.onchange = async () => {
-            const files = fileInput.files;
-            if (files && files.length > 0 && files[0].size > 0) {
-                const file = files[0];
+    fileInput.onchange = async () => {
+        const files = fileInput.files;
+        if (files && files.length > 0 && files[0].size > 0) {
+            const file = files[0];
 
-                const container = document.createElement("section");
-                container.classList = "file-wrapper";
+            const container = document.createElement("section");
+            container.classList = "file-wrapper";
 
-                const jsonFile = JSON.parse(await file.text());
+            const jsonFile = JSON.parse(await file.text());
 
-                importFile(jsonFile, kvMap);
+            importFile(jsonFile, kvMap);
 
-                if (kvMap.size > 0) {
-                    createTables(kvMap, container);
-                }
-
-                mainContainer?.appendChild(container);
-                mainContainer?.appendChild(saveButton);
+            if (kvMap.size > 0) {
+                createTables(kvMap, container);
             }
+
+            const saveEntryButton = document.createElement("button");
+            saveEntryButton.classList = "button primary-button";
+            saveEntryButton.textContent = "Save Entry";
+
+            container.appendChild(saveEntryButton);
+
+            newFileButton?.parentElement?.appendChild(saveAllButton);
+            newFileButton?.remove();
+            openFileButton?.remove();
+
+            mainContainer?.appendChild(container);
         }
-    })
-}
+    }
+})
 
 function createTables(set, container) {
     for (const subset of set) {
-        const frag = document.createDocumentFragment();
+        const wrapper = document.createElement("section");
+        wrapper.classList = "kv-wrapper";
+
+        const keyLabel = document.createElement("label");
+
         if (Array.isArray(subset[1]) || subset[1] instanceof Map) {
-            const wrapper = document.createElement("section");
-            wrapper.classList = "kv-wrapper";
-
-            const keyLabel = document.createElement("label");
             keyLabel.textContent = subset[0];
-
             wrapper.appendChild(keyLabel);
-
             createTables(subset[1], wrapper);
-
-            frag.appendChild(wrapper);
+            container.appendChild(wrapper);
         } else {
-            const wrapper = document.createElement("section");
-            wrapper.classList = "kv-wrapper";
-
-            const keyLabel = document.createElement("label");
-            keyLabel.textContent = subset[0];
-
             const keyValue = document.createElement("input");
             keyValue.value = subset[1].currentValue;
-            console.log(subset[1]);
 
             wrapper.appendChild(keyLabel);
             wrapper.appendChild(keyValue);
-
-            frag.appendChild(wrapper);
         }
-        container.appendChild(frag);
+        container.appendChild(wrapper);
     }
 }
 
